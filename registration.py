3f20fff8d49441cbf01c3938401845ebb1bc2fdf
umarb <umarbekkholmurodov@gmail.com>
2025-05-13 15:52:58 +0900

    Some changes

--- registration.py
+++ registration.py
@@ -5,23 +5,30 @@ class Registration:
         self.doctor = doctor
         self.patient = patient
         self.illness = illness
-
-    @property
-    def final_result(self):
-        return {
+        self.final_result = {
             "doctor": self.doctor,
             "patient": self.patient,
             "illness": self.illness
         }
+        self.filename = "sample_file.csv"
+    def get_data(self):
+        try:
+            data = pd.read_csv(self.filename)
+        except pd.errors.EmptyDataError:
+            data = pd.DataFrame(columns=["doctor", "patient", "illness"])
+            data.to_csv(self.filename, index=False)
+            data = pd.read_csv(self.filename)
+
+        new_row = pd.DataFrame([self.final_result])
+        data = pd.concat([data, new_row], ignore_index=True)
+
+        data.to_csv(self.filename, index=False)
+        return data
 
     def patient_register(self):
-        data = pd.read_csv("sample_file.csv")
-        data = pd.concat([data, pd.DataFrame([self.final_result])], ignore_index=True)
-        data.to_csv("sample_file.csv")
-        if self.doctor in data.doctor.to_list():
-            return "The registration was successful !"
+        data = self.get_data()
+        if self.doctor in data["doctor"].to_list():
+            return "The registration was successful!"
         else:
-            return "Error with registration !"
+            return "Error with registration!"
 
-sample_registration = Registration("house", "the-patient", "lung-flu")
-print(sample_registration.patient_register())
